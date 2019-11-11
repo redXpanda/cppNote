1bce35deae458abf9757c56feb0e0e6c58afe004
Marcus <Marcus@SYDZSB0493>
2019-11-11 20:35:16 +0800

    update

--- CMakeProject2/CMakeProject2.cpp
+++ CMakeProject2/CMakeProject2.cpp
@@ -3,10 +3,43 @@
 
 #include "CMakeProject2.h"
 
+#include <vector>
 using namespace std;
+
+std::vector<int> getRandomUnrepeatVec(int low, int high, int len)
+{
+	vector<int> out;
+	if (high > low && low >= 0)
+	{
+		if (len <= high - low + 1)	//0-5,6个数字
+		{
+			do
+			{
+				int n = (rand() % (high - low + 1)) + low;
+				if (find(out.begin(), out.end(), n) == out.end())
+				{
+					out.push_back(n);
+				}
+			} while (out.size() < len);
+
+		}
+	}
+
+
+
+	return out;
+}
+
+
+
 
 int main()
 {
 	cout << "Hello CMake。" << endl;
+
+	auto ret = getRandomUnrepeatVec(1, 8, 10);
+
+
+
 	return 0;
 }
